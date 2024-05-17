77f706afa95c4baf8f6d9bab86f70f52f0568250
Arijit Kundu <arikundu9@gmail.com>
2024-05-17 17:24:39 +0900

    logo

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -21,11 +21,11 @@ export default function Header() {
             <div className="container _._flex _._flex-wrap md:_._justify-between _._justify-center _._p-2 _._md:p-5">
                 <Link href="/" className="_._flex title-font _._font-medium _._items-center _._text-gray-900 _._mb-4">
                     {/* <img src="/new_logo.png" alt="logo" width="200px" height="auto" className="animate__animated animate__backInLeft animate__faster`" /> */}
-                    {/* <ExampleLogo /> */}
-                    <div className="logo_text">
+                    <ExampleLogo />
+                    {/* <div className="logo_text">
                         <span className="_._text-[#fe5855]">codex</span>
                         <span className="_._text-[#6721ff]">de</span>
-                    </div>
+                    </div> */}
                     {/* <span className="large">B</span>
                     <span className="large S">S</span>
                     <span className="nor-text Backlink">Backlink</span>
