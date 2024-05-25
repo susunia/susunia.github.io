d04a18306395108ad6680975225e4b3be29d9bd8
Arijit Kundu <arikundu9@gmail.com>
2024-05-25 19:49:15 +0530

    md--lg++

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -12,7 +12,7 @@ export default function Header() {
     const pathName = usePathname();
     return (
         <header>
-            <div className="container _._flex _._flex-wrap md:_._justify-between _._justify-center _._p-2 _._md:p-5">
+            <div className="container _._flex _._flex-wrap lg:_._justify-between _._justify-center _._p-2 _._md:p-5">
                 <Link href="/" className="_._flex title-font _._font-medium _._items-center _._text-gray-900 _._mb-4">
                     {/* <img src="/new_logo.png" alt="logo" width="200px" height="auto" className="animate__animated animate__backInLeft animate__faster`" /> */}
                     <ExampleLogo />
